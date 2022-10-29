bddbd873e28cdde0f8dcafb40e75229a892d82ac
Akash Sinha <lionelakash111@gmail.com>
2022-10-29 14:43:07 +0530

    Direct commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import TextArea from './components/TextArea';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [btnColor, setbtnColor] = useState('primary');
+
+  function darkMode(){
+    if(mode === "light" && btnColor === "primary"){
+      setMode("dark");
+      setbtnColor("dark");
+      document.body.style.backgroundColor = '#1f476b';
+    }
+    if(mode === "dark" && btnColor === "dark"){
+      setMode("light");
+      setbtnColor("primary");
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <div className="App">
-      <TextArea heading="Write something"/>
+      <TextArea heading="Write something" mode={mode} darkMode={darkMode}/>
     </div>
   );
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,56 +1,62 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 
 export default function TextArea(props) {
-    
-    const upperClickFunc = ()=>{
+
+    const upperClickFunc = () => {
         // console.log("Uppercase button was clicked",text); / * debugging purpose
         let newText = text.toUpperCase();
         setText(newText);
     };
 
-    const lowerClickFunc = ()=>{
+    const lowerClickFunc = () => {
         let newText = text.toLowerCase();
         setText(newText);
     };
 
-   
-    const copyText = ()=>{
+
+    const copyText = () => {
         var text = document.getElementById("textarea1");
         text.select();
         navigator.clipboard.writeText(text.value);
     };
 
-    const clearFunc = ()=>{
+    const clearFunc = () => {
         setText("");
     };
 
-    const onChangeHandle = (event)=>{
+    const onChangeHandle = (event) => {
         // console.log("Handle onchange"); /* debugging purpose
         setText(event.target.value);
     };
 
     const [text, setText] = useState("");
     return (
-         <>
-        <div className='container'>
-            <label htmlFor="exampleFormControlTextarea1"><b> {props.heading} </b></label>
-            <div className="mb-3 my-3">
-                <textarea className="form-control" value={text} onChange={onChangeHandle} id="textarea1" rows="9"></textarea>
-                <div>
-                <button className='btn btn-outline-primary my-3' onClick={copyText}> <i> Copy text </i> </button>
+        <>
+            <div className="container my-3" >
+                <label htmlFor="exampleFormControlTextarea1" style={{color: props.mode === 'dark'? 'white' : 'black'}}><b> {props.heading} </b></label>
+                <div className="mb-3 my-3">
+                    <textarea className={`form-control bg-${props.mode}`} style={{color: props.mode === 'dark'? 'white' : 'black'}} value={text} onChange={onChangeHandle} id="textarea1" rows="9"></textarea>
+
+                    <div>
+                        <button className={`btn btn-outline-${props.btnColor} my-3`} onClick={copyText}> <i> Copy text </i> </button>
+                    </div>
+                    <button className={`btn btn-outline-${props.btnColor} my-3`} onClick={upperClickFunc}> To Uppercase</button>
+                    <button className={`btn btn-outline-${props.btnColor} mx-1 my-3`} onClick={lowerClickFunc}> To Lowercase</button>
+                    <button className="btn btn-outline-danger mx-1 my-3" onClick={clearFunc}> Clear </button>
                 </div>
-                <button className='btn btn-outline-primary my-3' onClick={upperClickFunc}> To Uppercase</button>
-                <button className='btn btn-outline-primary mx-1 my-3' onClick={lowerClickFunc}> To Lowercase</button>
-                <button className='btn btn-outline-danger mx-1 my-3' onClick={clearFunc}> Clear </button>
+                
+                <div className="{form-check form-switch}">
+                    <input clasNameclassName="form-check-input" onClick={props.darkMode} type="checkbox" id="flexSwitchCheckDefault" />
+                    <label className="form-check-label" for="flexSwitchCheckDefault">Dark mode</label>
+                </div>
             </div>
-        </div>
-        <div className="container1 mx-1">
-            <h5> Text analysis:</h5>
-            <p> <b> {text.split(" ").length} </b> words and characters <b> {text.length} </b> </p>
-            <p> <b> {0.008 * text.split(" ").length} </b> Min to read</p>
-            <h5> Prev.</h5>
-            <p> {text} </p>
-        </div>
-         </>
+            <div className="container1 mx-1" style={{color: props.mode === 'dark'? 'white': 'black'}}>
+                <h5> Text analysis:</h5>
+                <p> <b> {text.split(" ").length} </b> words and characters <b> {text.length} </b> </p>
+                <p> <b> {0.008 * text.split(" ").length} </b> Min to read</p>
+                <h5> Prev.</h5>
+                <p> {text} </p>
+            </div>
+        </>
     )
 }
